d77ec9a525add91e136f0a6e95a0c18766459314
peteranton1 <peteranton1@yahoo.com>
2024-08-04 13:12:10 +0100

    add swagger

--- coffeeshop/src/main/java/com/example/coffeeshop/controller/UserController.java
+++ coffeeshop/src/main/java/com/example/coffeeshop/controller/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
   private final UserService userService;
